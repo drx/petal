3cbf7001380ce596e6c0e21417835776a42286c8
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-02-13 13:16:52 +0100

    fixin #1

--- Interpreter.hs
+++ Interpreter.hs
@@ -52,66 +52,66 @@ replace a v n = let (f,s) = splitAt n a in (init f) ++ (v:s)
 
 step :: State -> Maybe State
 step (heap, rf, iss) = case iss of
-    Seq s ((Assign r1 v):is) jv rs -> case rhat rf v of
+    Seq s ((Assign r1 v):is) jv rs t -> case rhat rf v of
         UPointer h  -> error $ "Illegal unique pointer access"
-        _           -> Just (heap, substreg rf r1 v, Seq s is jv rs)
+        _           -> Just (heap, substreg rf r1 v, Seq s is jv rs t)
     
-    Seq s ((Malloc r1 n):is) jv rs -> Just (heap, substreg rf r1 (UPointer $ Tup (replicate n (Int 0))),Seq s is jv rs)
+    Seq s ((Malloc r1 n):is) jv rs t-> Just (heap, substreg rf r1 (UPointer $ Tup (replicate n (Int 0))),Seq s is jv rs t)
 
-    Seq s ((Commit r1):is) jv rs | isStackPointer r1 -> error $ "Can't commit a stack pointer"
-                                 | otherwise         -> case rhat rf (Register r1) of
+    Seq s ((Commit r1):is) jv rs t| isStackPointer r1 -> error $ "Can't commit a stack pointer"
+                                  | otherwise         -> case rhat rf (Register r1) of
                                                             UPointer h  -> let l = uniqueLabel heap 
-                                                                            in Just ((l,h):heap, substreg rf r1 (Label l), Seq s is jv rs)
+                                                                            in Just ((l,h):heap, substreg rf r1 (Label l), Seq s is jv rs t)
                                                             _           -> error $ "Can't commit something else than unique pointer"
-    Seq s ((Load r1 r2 n):is) jv rs -> case r rf r2 of
+    Seq s ((Load r1 r2 n):is) jv rs t -> case r rf r2 of
         UPointer h -> case h of
             HeapSeq _ -> error "Loading data from code label"
-            Tup t -> Just (heap, substreg rf r1 (t !! (n - 1)), Seq s is jv rs)
+            Tup tp -> Just (heap, substreg rf r1 (tp !! (n - 1)), Seq s is jv rs t)
         Label l -> case  h rf heap (Label l) of
             HeapSeq _ -> error "Loading data from codo label"
-            Tup t -> Just (heap, substreg rf r1 (t !! (n - 1)), Seq s is jv rs)
+            Tup tp -> Just (heap, substreg rf r1 (tp !! (n - 1)), Seq s is jv rs t)
 
-    Seq s ((Save r1 r2 n):is) jv rs -> case r rf r2 of
+    Seq s ((Save r1 r2 n):is) jv rs t -> case r rf r2 of
         UPointer h -> case h of
             HeapSeq _ -> error "Saving data under code label"
-            Tup t -> case (r rf r1) of
+            Tup tp -> case (r rf r1) of
                 UPointer _ -> error "Saving data under unique pointer"
-                v -> Just (heap, substreg rf r2 (UPointer (Tup (replace t v n))), Seq s is jv rs)
+                v -> Just (heap, substreg rf r2 (UPointer (Tup (replace tp v n))), Seq s is jv rs t)
         Label l -> case  h rf heap (Label l) of
             HeapSeq _ -> error "Saving data under codo label"
-            Tup t ->  case (r rf r1) of
+            Tup tp ->  case (r rf r1) of
                 UPointer _ -> error "Saving data under unique pointer"
-                v -> Just (substheap heap l (Tup t), rf, Seq s is jv rs)
+                v -> Just (substheap heap l (Tup tp), rf, Seq s is jv rs t)
         v -> error $ "Some error " ++ (show v)
 
-    Seq s ((Salloc n):is) jv rs -> case r rf 0 of
+    Seq s ((Salloc n):is) jv rs t -> case r rf 0 of
         UPointer h -> case h of
             HeapSeq _ -> error "Stack pointer pointing to a code label"
-            Tup t   | (length t) + n > maxstack -> error "Stack overflow"
-                    | otherwise -> Just (heap, substreg rf 0 (UPointer $ Tup $ (replicate n (Int 0)) ++ t), Seq s is jv rs)
+            Tup tp  | (length tp) + n > maxstack -> error "Stack overflow"
+                    | otherwise -> Just (heap, substreg rf 0 (UPointer $ Tup $ (replicate n (Int 0)) ++ tp), Seq s is jv rs t)
         _ -> error "Stack pointer containing something else than unique pointer"
 
-    Seq s ((Sfree n):is) jv rs -> case r rf 0 of
+    Seq s ((Sfree n):is) jv rs t -> case r rf 0 of
         UPointer h -> case h of
             HeapSeq _ -> error "Stack pointer pointing to a code label"
-            Tup t -> Just (heap, substreg rf 0 (UPointer $ Tup $ (drop n t)), Seq s is jv rs)
+            Tup tp -> Just (heap, substreg rf 0 (UPointer $ Tup $ (drop n tp)), Seq s is jv rs t)
         _ -> error "Stack pointer containing something else than unique pointer"
         
 
-    Seq s ((AssignPlus r1 r2 v):is) jv rs -> Just (heap,
+    Seq s ((AssignPlus r1 r2 v):is) jv rs t -> Just (heap,
                                                 substreg rf r1 (Int ((getValue  $ r rf r2) + (getValue $ rhat rf v))),
-                                                Seq s is jv rs)
+                                                Seq s is jv rs t)
 
-    Seq s ((IfJump r1 v):is) jv rs -> case getValue $ r rf r1 of
+    Seq s ((IfJump r1 v):is) jv rs t -> case getValue $ r rf r1 of
         0 ->    case v of
                 Label l | l == exit -> Nothing
                 _ -> case h rf heap (rhat rf v) of
                     HeapSeq i -> Just (heap, rf, i)
                     Tup _ -> error "Jumping to a data label"
 
-        n ->    Just (heap, rf, Seq s is jv rs)
+        n ->    Just (heap, rf, Seq s is jv rs t)
 
-    Seq s [] jv rs -> case jv of
+    Seq s [] jv rs t -> case jv of
         Label l | l == exit -> Nothing
         _ -> case  h rf heap (rhat rf jv) of
             HeapSeq i -> Just (heap, rf, i)
