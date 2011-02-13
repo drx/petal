62d2f5337c436f114a7719846ec539e47918700e
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-02-13 02:33:09 +0100

    shows for instructions/sequences

--- Syntax/Term.hs
+++ Syntax/Term.hs
@@ -13,10 +13,10 @@ isStackPointer  0 = True
 isStackPointer _ = False
 
 instance Show Value where
-        show (Int n) = (show n)
-        show (Label l) = l
-        show (Register n) = "r" ++ (show n)
-        show (UPointer h) = "uptr(" ++ (show h) ++ ")"
+    show (Int n) = (show n)
+    show (Label l) = l
+    show (Register n) = "r" ++ (show n)
+    show (UPointer h) = "uptr(" ++ (show h) ++ ")"
 
 data Instruction =        Assign {  getAssignDestination :: Int,
                                     getAssignedValue :: Value }
@@ -37,7 +37,19 @@ data Instruction =        Assign {  getAssignDestination :: Int,
                         | Commit {  getCommitRegister :: Int }
                         | Sfree  {  getSfreeCount :: Int }
                         | Salloc {  getSallocCount :: Int }
-                        deriving (Show, Eq)
+                        deriving (Eq)
+
+instance Show Instruction where
+   show (Assign r v) = "r" ++ (show r) ++ " = " ++ (show v)
+   show (AssignPlus r r1 v) = "r" ++ (show r) ++ " = r" ++ (show r1) ++ " + " ++ (show v)
+   show (IfJump r v) = "if (r" ++ (show r) ++ " == 0) jump " ++ (show v)
+   show (Jump v) = "jump " ++ (show v)
+   show (Load r1 r2 n) = "r" ++ (show r1) ++ " = mem[r" ++ (show r2) ++ " + " ++ (show n) ++ "]"
+   show (Save r1 r2 n) = "mem[r" ++ (show r2) ++ " + " ++ (show n) ++ "] = r" ++ (show r1)
+   show (Malloc r n) = "r" ++ (show r) ++ " = malloc " ++ (show n)
+   show (Commit r) = "commit r" ++ (show r)
+   show (Sfree n) = "sfree " ++ (show n)
+   show (Salloc n) = "salloc " ++ (show n)
 
 type Heap = [(String, HeapValue)]
 type RegisterFile = [(Int, Value)]
@@ -59,7 +71,14 @@ data InstructionSequence = Seq {    getName :: String,
                                     getCode :: [Instruction],
                                     getJump :: Value,
                                     getRegisters :: [Int] } 
-                                    deriving (Show, Eq)
+                                    deriving (Eq)
+
+instance Show InstructionSequence where
+    show (Seq s is v rs) = s ++ "(" ++ (showRegisters rs) ++ "):\n" ++
+                            (unlines (map show is)) ++
+                            "jump " ++ (show v) where
+                                showRegisters (r:rs) | rs /= [] = "r" ++ (show r) ++ ", " ++ (showRegisters rs)
+                                                     | otherwise = "r" ++ (show r)
 
 type Program = [InstructionSequence]
 
